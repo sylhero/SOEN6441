bc03829f76a14e1424a22491f7d0f2ce1ec69f83
Yichen LI <me.castiel@gmail.com>
2014-09-30 12:09:18 -0400

    ValidatePath function added
    Part of the ValidatePath function implemented

--- SOEN6441/src/usefulfunctions/ValidateMap.java
+++ SOEN6441/src/usefulfunctions/ValidateMap.java
@@ -7,11 +7,17 @@ import tilemap.TileMap;
  * 
  * @author Yichen LI
  *
+ *
  */
 public class ValidateMap {
 
 	private static int entranceCount = 0;
 	private static int exitCount = 0;
+	private static int [][] pathGrid;
+	private final static int CHECKED = 20;
+	private final static int VALID_PATH = 30;
+	
+	//======================================validate the entry point==========================================
 	
 	public static boolean validateEntrace(Tile [][] map)
 	{
@@ -35,8 +41,7 @@ public class ValidateMap {
 	}
 	
 	
-	//----------------------------------------------------------------------------------------------------------------------------
-	
+	//=========================================validate the exit point==============================================
 	
 	public static boolean validateExit(Tile [][] map)
 	{
@@ -57,7 +62,57 @@ public class ValidateMap {
 			return false;
 	}
 	
-	//--------------------------------------------------------------------------------------------------------------------------------
+	//==========================================validate the path ========================================================
+	
+	public static boolean validatePath(int row, int column)
+	{
+		
+		boolean done = false;
+		
+		if(valide (row, column))
+		{
+			pathGrid[row][column] = CHECKED; // indicate the tile is checked
+			
+			if(pathGrid[row][column] == TileMap.EXIT || (row == pathGrid.length - 1 && column == pathGrid[0].length - 1))
+				done = true;
+			else
+			{
+				done = validatePath(row + 1, column);  // down	
+				if(!done)
+					done = validatePath(row, column + 1); // right
+				if(!done)
+					done = validatePath(row - 1, column); // up
+				if(!done)
+					done = validatePath(row, column - 1); // left
+			}
+			
+			if(done) // the location is part of the path
+				pathGrid[row][column] = VALID_PATH; 
+			
+		}
+		return false;
+		
+	}
+
+	
+	//---------------------------------------------
+	// determine if specific location is valid
+	//---------------------------------------------
+
+	private static boolean valide(int row, int column) 
+	{
+		boolean result = false;
+		
+		// check if the tile is in the bounds of the map
+		if(row >= 0 && row < pathGrid.length && column >= 0 && column < pathGrid[row].length)
+		{
+			// check if the tile is not blocked and not previously checked
+			if(pathGrid[row][column] == TileMap.PAVEMENT)
+				result = true;
+		}
+				
+		return result;
+	}
 }
 
 
