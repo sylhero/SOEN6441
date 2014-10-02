fac72c9648510aaf6ea73615cbe039314fdb2934
Yichen LI <me.castiel@gmail.com>
2014-10-02 17:20:26 -0400

    ValidateMap class implemented (not tested)
    added validatePath function which uses a boolean [ ][ ] to store correct
    path

--- SOEN6441/src/usefulfunctions/ValidateMap.java
+++ SOEN6441/src/usefulfunctions/ValidateMap.java
@@ -13,9 +13,12 @@ public class ValidateMap {
 
 	private static int entranceCount = 0;
 	private static int exitCount = 0;
-	private static int [][] pathGrid;
-	private final static int CHECKED = 20;
-	private final static int VALID_PATH = 30;
+	
+	private int entryX, entryY, exitX, exitY;
+	private int width, height;
+	private int localMap [][];
+	private boolean correctPath [][];
+	private boolean wasHere [][];
 	
 	//======================================validate the entry point==========================================
 	
@@ -23,14 +26,12 @@ public class ValidateMap {
 	{
 		
 		for (int i = 0; i < map.length; i++)
-		{
 			for (int j = 0; j < map[i].length; j++)
 			{			
 				if(map[i][j].getTileType() == TileMap.ENTRANCE)
 				{
 					entranceCount++;
-				}
-			}		
+				}	
 		}
 		
 		if(entranceCount == 1) // if only one entrance exists
@@ -46,14 +47,12 @@ public class ValidateMap {
 	public static boolean validateExit(Tile [][] map)
 	{
 		for (int i = 0; i < map.length; i++)
-		{
 			for (int j = 0; j < map[i].length; j++)
 			{			
 				if(map[i][j].getTileType() == TileMap.EXIT)
 				{
 					exitCount++;
-				}
-			}		
+				}	
 		}
 		
 		if(exitCount == 1)  // if only one exit exists
@@ -64,6 +63,131 @@ public class ValidateMap {
 	
 	//==========================================validate the path ========================================================
 	
+	public boolean validatePath(Tile [][] map)
+	{
+		init(map); // initialize parameters 
+		
+		boolean result = recursiveSolve(entryX, entryY);
+		// will leave with a boolean array(correctPath)
+		// with the path indicated by true values.
+		// if result is false, there is no path to the exit
+		
+		return result;
+	}
+	
+	
+	private boolean recursiveSolve(int x, int y) {
+	
+		if(x == exitX && y == exitY) return true; // if reached the exit
+		if(localMap[x][y] == TileMap.GRASS || wasHere[x][y])  return false; // if encountered grass or already been here
+		
+		wasHere[x][y] = true;
+		
+		if(x != 0) // checks if not on Left edge
+			if(recursiveSolve(x - 1, y)) // recall method one to the left
+			{
+				correctPath[x][y] = true;
+				return true;
+			}
+		
+		if(x != width - 1) // check if not on right edge
+			if(recursiveSolve(x + 1, y)) // recall method one to the right
+			{
+				correctPath[x][y] = true;
+				return true;
+			}
+		
+		if(y != 0) // check if not on top edge
+			if(recursiveSolve(x, y - 1)) // recall method one up
+			{
+				correctPath[x][y] = true;
+				return true;
+			}
+		
+		if(y != height -1) // check if not on bottom edge
+			if(recursiveSolve(x, y + 1)) // recall method one bottom
+			{
+				correctPath[x][y] = true;
+				return true;
+			}
+		
+		return false;
+	}
+
+
+	private void init(Tile [][]map) {
+			
+		for(int row = 0; row < map.length; ++row)
+			for(int col = 0; col < map[row].length; ++col)
+			{
+				if(map[row][col].getTileType() == TileMap.ENTRANCE)
+				{
+					entryX = row; // record entry coordinates 
+					entryY = col;
+				}
+					
+				if(map[row][col].getTileType() == TileMap.EXIT)
+				{
+					exitX = row;  // record exit coordinates
+					exitY = col;
+				}
+				
+				width = row + 1;
+				height = col + 1;
+			}
+
+		// initialize localMap, wasHere and correctPath
+		localMap = new int[width][height];
+		
+		// generate localMap
+		generate(map);
+		
+		wasHere = new boolean[width][height];
+		correctPath = new boolean[width][height];
+		
+		// set boolean Arrays to default values
+		for(int row = 0; row < map.length; ++row)
+			for(int col = 0; col < map[row].length; ++col)
+			{
+				wasHere[row][col] = false;
+				correctPath[row][col] = false;
+			}
+		
+		
+	}
+
+	// copy values from map to localMap
+	private void generate(Tile [][] map) {
+		
+		for(int row = 0; row < map.length; ++row)
+			for(int col = 0; col < map[row].length; ++col)
+			{
+				localMap[row][col] = map[row][col].getTileType();
+			}
+	}
+	
+	
+	
+	public static void main(String args[])
+	{
+		
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	/*
 	public static boolean validatePath(int row, int column)
 	{
 		
@@ -90,7 +214,7 @@ public class ValidateMap {
 				pathGrid[row][column] = VALID_PATH; 
 			
 		}
-		return false;
+		return done;
 		
 	}
 
@@ -112,7 +236,7 @@ public class ValidateMap {
 		}
 				
 		return result;
-	}
+	}*/
 }
 
 
