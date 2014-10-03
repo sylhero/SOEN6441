e162706c21d8cb8e089afca937fcbd994776b2ba
Hongrui Guan <guan.hongrui@gmail.com>
2014-10-02 22:22:10 -0400

    Added Javadoc for class MapParser Added comments for class MapParser

--- SOEN6441/src/xml/MapParser.java
+++ SOEN6441/src/xml/MapParser.java
@@ -20,19 +20,31 @@ import tilemap.Tile;
 import usefulfunctions.LoadImage;
 
 /**
+ * This class implements XML write and parse functions.
+ * This class will use for saving a customized map into a XML file and transform XML file
+ * for reloading an existing map on the map editor panel
+ *  
  * @author Hongrui Guan
  * @date 2014-09-24 
- * This class created for both saving a customized map into a XML File
- * and parsing XML File for reloading an existing map on the map editor panel
+ *
  */
 public class MapParser {
 
+	//The root element read from a XML file
 	private Element xmlFile;
 
+	/**
+	 * This method saves a map information to a XML file.
+	 * 
+	 * @param mapInformation All tiles information in the map. 
+	 * @param mapName The map name that user named for
+	 * @return Return true if XML file creating succeed
+	 * */
 	public boolean createXMLFile(Tile[][] mapInformation, String mapName)
 			throws IOException {
-		// TODO catch exception
+
 		Document document = DocumentHelper.createDocument();
+		
 		Element mapFile = document.addElement("map");
 		Element fileName = mapFile.addElement("fileName");
 		fileName.setText(mapName);
@@ -41,7 +53,6 @@ public class MapParser {
 		Element mapColumn = mapFile.addElement("mapColumn");
 		mapColumn.setText(String.valueOf(mapInformation[0].length));
 		Element mapData = mapFile.addElement("mapData");
-		
 		int mapSize = mapInformation.length*mapInformation[0].length;
 		Element[] tiles = new Element[mapSize];
 		for (int i = 0; i < mapInformation.length; i++) {
@@ -62,6 +73,7 @@ public class MapParser {
 				
 			}
 		}
+		//System clock will be added into the file name to make its unique. 
 		Date createDate = new Date();
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yymmddhhmm");
 		String mapDate = dateFormat.format(createDate);
@@ -70,29 +82,48 @@ public class MapParser {
 				+ ".xml");
 	}
 
-	public void loadXMLFile(String mapDirectory) throws DocumentException {
+	/**
+	 * This method loads XML file and gets the root element of XML file. 
+	 * This method should be invoked before methods getMapName and getMapData.
+	 * The root element will be saved as a property of class.
+	 * 
+	 * @param mapDirectory
+	 */
+	public void loadXMLFile(String mapDirectory){
 		
 		SAXReader reader = new SAXReader();
-		Document xmlDocument = reader.read(mapDirectory);
-
-		// TODO check if xml file is empty
-		Element xmlRoot = xmlDocument.getRootElement();
-		this.xmlFile = xmlRoot;
+		
+		try{
+			Document xmlDocument = reader.read(mapDirectory);
+			Element xmlRoot = xmlDocument.getRootElement();
+			this.xmlFile = xmlRoot;
+		}catch(DocumentException e){
+			// auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
-	public String getMapName() throws DocumentException {
+	/**
+	 * This method acquires map name from the root element of XML file.
+	 * 
+	 * @return mapName The map name will be returned as a string.
+	 * */
+	public String getMapName(){
+		
 		String mapName = xmlFile.elementText("fileName");
-
 		System.out.println("map name is " + mapName);
 
 		return mapName;
 	}
 
-	public Tile[][] getMapData() throws NumberFormatException,
-	DocumentException {
+	/**
+	 * This method acquires map data from the root element of XML file.
+	 * 
+	 * @return tiles All tiles will be returned as a Tile array.
+	 * */
+	public Tile[][] getMapData(){
 		
-
 		Image grass       = LoadImage.loadImage("/images/grass.png");
 		Image pavement    = LoadImage.loadImage("/images/pavement.png");
 		Image entrance    = LoadImage.loadImage("/images/entrance.png");
@@ -105,58 +136,75 @@ public class MapParser {
 		int row = Integer.parseInt(xmlFile.elementText("mapRow"));
 		int column = Integer.parseInt(xmlFile.elementText("mapColumn"));
 		Tile[][] tiles = new Tile[row][column];
-
-		Element mapData = xmlFile.element("mapData");
-		List<Element> tileData = mapData.elements("tile");
-		System.out.println("tile size is "+tileData.size());
 		
-		System.out.println("map height is equal to " + row);
-		System.out.println("map width is equal to " + column);
-		System.out.println("map data is");
+		try{
+			Element mapData = xmlFile.element("mapData");
+			List<Element> tileData = mapData.elements("tile");
+			
+			System.out.println("tile size is "+tileData.size());		
+			System.out.println("map height is equal to " + row);
+			System.out.println("map width is equal to " + column);
+			System.out.println("map data is");
 		
-		for (int i = 0; i < row; i++) {
-			for (int j = 0; j < column; j++) {
-				tiles[i][j] = new Tile();
-				tiles[i][j].setTileType((Integer.parseInt(tileData.get(i*column+j).elementText("tileType"))));
-				switch(tiles[i][j].getTileType()) {
+			//set all attributes of tile into the Tile array
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < column; j++) {
+					tiles[i][j] = new Tile();
+					tiles[i][j].setTileType((Integer.parseInt(tileData.get(i*column+j).elementText("tileType"))));
+					//write type image into the Tile array based on tile type
+					switch(tiles[i][j].getTileType()) {
 				
-				case GRASS:
-					tiles[i][j].setTileImage(grass);
-					break;
-				case ENTRANCE:
-					tiles[i][j].setTileImage(entrance);
-					break;
-				case PAVEMENT:
-					tiles[i][j].setTileImage(pavement);
-					break;
-				case EXIT:
-					tiles[i][j].setTileImage(exit);
-					break;
+					case GRASS:
+						tiles[i][j].setTileImage(grass);
+						break;
+					case ENTRANCE:
+						tiles[i][j].setTileImage(entrance);
+						break;
+					case PAVEMENT:
+						tiles[i][j].setTileImage(pavement);
+						break;
+					case EXIT:
+						tiles[i][j].setTileImage(exit);
+						break;
 					
-				}
+					}
 				
-				tiles[i][j].setTileX((Integer.parseInt(tileData.get(i*column+j).elementText("tileX"))));
-				tiles[i][j].setTileY((Integer.parseInt(tileData.get(i*column+j).elementText("tileY"))));
-				tiles[i][j].setTileWidth((Integer.parseInt(tileData.get(i*column+j).elementText("tileWidth"))));
-				tiles[i][j].setTileHeight((Integer.parseInt(tileData.get(i*column+j).elementText("tileHeight"))));
-				System.out.println("Tile Type:"+tiles[i][j].getTileType()+",Tile Image:"+tiles[i][j].getTileImage().toString());
+					tiles[i][j].setTileX((Integer.parseInt(tileData.get(i*column+j).elementText("tileX"))));
+					tiles[i][j].setTileY((Integer.parseInt(tileData.get(i*column+j).elementText("tileY"))));
+					tiles[i][j].setTileWidth((Integer.parseInt(tileData.get(i*column+j).elementText("tileWidth"))));
+					tiles[i][j].setTileHeight((Integer.parseInt(tileData.get(i*column+j).elementText("tileHeight"))));
+					System.out.println("Tile Type:"+tiles[i][j].getTileType()+",Tile Image:"+tiles[i][j].getTileImage().toString());
+				}
 			}
+		}catch(NumberFormatException e){
+			e.printStackTrace();
 		}
 
 		return tiles;
 	}
 
-
-	private boolean writeFormatXML(Document xmlDocument, String mapDirectory)
-			throws IOException {
+	/**
+	 * This method write a format XML file.
+	 * This method will be invoked on method createXMLFile.
+	 * 
+	 * @param xmlDocument 
+	 * @param mapDirectory
+	 * @return Return true if xml file creating succeed
+	 * */
+	private boolean writeFormatXML(Document xmlDocument, String mapDirectory){
+		
 		boolean isTrue = false;
-		XMLWriter writer = null;
-		OutputFormat format = OutputFormat.createPrettyPrint();
-		format.setEncoding("UTF-8");
-		writer = new XMLWriter(new FileWriter(new File(mapDirectory)), format);
-		writer.write(xmlDocument);
-		writer.close();
-		isTrue = true;
+		try{
+			XMLWriter writer = null;
+			OutputFormat format = OutputFormat.createPrettyPrint();
+			format.setEncoding("UTF-8");
+			writer = new XMLWriter(new FileWriter(new File(mapDirectory)), format);
+			writer.write(xmlDocument);
+			writer.close();
+			isTrue = true;
+		}catch(IOException e){
+			System.out.println(e.getMessage());
+		}
 
 		return isTrue;
 	}
