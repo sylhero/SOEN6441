97e01072eb983b6b1b23248886111b179f571f51
Hongrui Guan <guan.hongrui@gmail.com>
2014-11-09 15:11:23 -0500

    Implement NearestStrategy

--- SOEN6441/src/towerstrategy/NearestStrategy.java
+++ SOEN6441/src/towerstrategy/NearestStrategy.java
@@ -1,7 +1,9 @@
 package towerstrategy;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
+import tilemap.TileMap;
 import towers.TowerBase;
 import critters.CritterBase;
 
@@ -15,14 +17,34 @@ public class NearestStrategy implements Strategy{
 	}
 
 	@Override
-	public CritterBase executeStrategy(ArrayList<CritterBase> al,
-			TowerBase towerShooter) {
+	public CritterBase executeStrategy(ArrayList<CritterBase> al,TowerBase towerShooter) {
 		// TODO Auto-generated method stub
-		return null;
+		Iterator<CritterBase> it = al.iterator();
+		double nearestDistance = 0;
+		CritterBase nearestCritter = null;
+		while(it.hasNext())
+		{
+			CritterBase critter = (CritterBase) it.next();
+			int critterX = critter.getX();
+			int critterY = critter.getY();
+			int critterCenterX = critterX + TileMap.getTileMap().getCellHeight() / 2;
+			int critterCenterY = critterY + TileMap.getTileMap().getCellWidth() / 2;
+			int towerCenterX   = towerShooter.getTileX() + TileMap.getTileMap().getCellHeight() / 2;
+			int towerCenterY   = towerShooter.getTileY() + TileMap.getTileMap().getCellWidth() / 2;
+			double distance = Math.sqrt(Math.pow(critterCenterX-towerCenterX, 2) + 
+					Math.pow(critterCenterY-towerCenterY, 2));
+			
+			if (nearestDistance == 0) 
+			{
+				nearestDistance = distance;
+			} 
+			else if (distance < nearestDistance)
+			{
+				nearestDistance = distance;
+				nearestCritter = critter;
+			}
+			
+		}
+		return nearestCritter;
 	}
-
-	
-	
-
-
 }
