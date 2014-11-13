f4693b5d34094f2c03485f4f0b7f44799958a53e
Yichen LI <me.castiel@gmail.com>
2014-11-12 22:43:17 -0500

    Code refactored

--- SOEN6441/src/towerstrategy/StrongestStrategy.java
+++ SOEN6441/src/towerstrategy/StrongestStrategy.java
@@ -2,6 +2,7 @@ package towerstrategy;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import towers.TowerBase;
 import critters.CritterBase;
@@ -20,8 +21,17 @@ public class StrongestStrategy implements Strategy{
 	@Override
 	public CritterBase executeStrategy(ArrayList<CritterBase> al,
 			TowerBase towerShooter) {
-		// TODO Auto-generated method stub
-		return Collections.max(al, new ComparatorByHp());
+	
+		return Collections.max(al, new Comparator<CritterBase>(){
+
+			@Override
+			public int compare(CritterBase arg0, CritterBase arg1) 
+			{		
+				int temp = arg0.getCurrentHp() - arg1.getCurrentHp();				
+				return (int) (temp == 0 ? arg0.getArmorRatio() - arg1.getArmorRatio() : temp);
+			}
+			
+		});
 	}
 
 
--- SOEN6441/src/towerstrategy/WeakestStrategy.java
+++ SOEN6441/src/towerstrategy/WeakestStrategy.java
@@ -2,6 +2,7 @@ package towerstrategy;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 
 import towers.TowerBase;
 import critters.CritterBase;
@@ -18,9 +19,18 @@ public class WeakestStrategy implements Strategy{
 		
 	@Override
 	public CritterBase executeStrategy(ArrayList<CritterBase> al,
-			TowerBase towerShooter) {
-		// TODO Auto-generated method stub
-		return Collections.min(al, new ComparatorByHp());
+			TowerBase towerShooter)	{
+
+		return Collections.min(al, new Comparator<CritterBase>(){
+
+			@Override
+			public int compare(CritterBase arg0, CritterBase arg1) 
+			{
+				int temp = arg0.getCurrentHp() - arg1.getCurrentHp();
+				return (int) (temp == 0 ? arg0.getArmorRatio() - arg1.getArmorRatio() : temp);
+			}
+			
+		});
 	}
 
 
