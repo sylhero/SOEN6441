63c6234b502dc259ff489c663980ad68eab07107
Hongrui Guan <guan.hongrui@gmail.com>
2014-11-21 22:03:07 -0500

    Fix bugs

--- SOEN6441/src/gamestate/LoadGameState.java
+++ SOEN6441/src/gamestate/LoadGameState.java
@@ -58,7 +58,7 @@ public class LoadGameState extends GameState{
 			//load the map first
 			
 			Tile[][] tempMap = gameData.ReadGameData(file.getName()).getMapData();
-			Coin.getCoinObject().setCurrency(gameData.getMoney());
+			Coin.getCoinObject().setCurrency(gameData.ReadGameData(file.getName()).getMoney());
 			tileMap.loadGameDataMap(file.getName(), tempMap);
 			//System.out.println(tileMap.getCellHeight());
 			tileMap.setCorrectPath(ValidateMap.getCorrectRoute(tempMap));
