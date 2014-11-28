10a424f881fa47ad8dd7ea49e94dbc09820de366
Yulong Song <felixsong88@gmail.com>
2014-11-28 12:07:39 -0500

    I wrote a great amount of test cases currently three.

---
+++ SOEN6441/src/junittest/MapLogTest.java
@@ -0,0 +1,62 @@
+package junittest;
+
+import static org.junit.Assert.*;
+import log.MapLog;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+/**
+ * this class is responsible for testing the map log class
+ * @author yulongsong
+ *
+ */
+public class MapLogTest {
+	private String name;
+	/**
+	 * set up the mag log object before testing
+	 */
+	@BeforeClass
+	public static void setUpMapLog(){
+		String name = "MapLogTestOnly";
+		MapLog.getMapLogObject().createMapLog(name);
+	
+	
+	}
+	/**
+	 * set up the name
+	 */
+	@Before
+	public void setUp(){
+		name = "MapLogTestOnly";
+		
+	}
+	/**
+	 * test the top five scrore
+	 */
+	
+	@Test
+	public void testReadTopFiveScore(){
+		int size = MapLog.getMapLogObject().getTopFive(name).size();
+		assertEquals(6,size);
+	}
+	/**
+	 * test get all logs
+	 */
+	
+	@Test 
+	public void testGetAllLogs(){
+		assertNotNull(MapLog.getMapLogObject().getAllMapLog(name));
+	}
+	/**
+	 * test get map log name
+	 */
+	
+	@Test 
+	public void testGetMapLogName(){
+		assertEquals("MapLogTestOnly",MapLog.getMapLogObject().getMapName());	
+	}
+	
+	
+	
+
+}
